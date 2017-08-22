26c85149fc9f5754fa180bc8ec97d4191c1273ab
Daniel Antrim <dantrim@uci.edu>
2017-08-22 05:01:43 -0700

    add shape plotting script

---
+++ python/analysis/wwbb/configuration/config_wwbb_shape.py
@@ -0,0 +1,201 @@
+import sys
+
+import dantrimania.python.analysis.utility.samples.sample as sample
+import dantrimania.python.analysis.utility.samples.region as region
+import dantrimania.python.analysis.utility.plotting.m_py.hist1d as hist1d
+
+##################################################################################
+# sample definition
+##################################################################################
+filelist_dir = "/data/uclhc/uci/user/dantrim/n0234val/filelists/"
+h5_dir_mc = "/data/uclhc/uci/user/dantrim/ntuples/n0234/c_aug16/mc/h5/"
+h5_dir_data = "/data/uclhc/uci/user/dantrim/ntuples/n0234/c_aug16/data/h5/"
+
+#loaded_samples = []
+#loaded_signals = []
+
+lumi_factor = 36.06
+
+# backgrounds
+ttbar = sample.Sample("ttbar", "$t\\bar{t}$")
+ttbar.scalefactor = lumi_factor 
+ttbar.fillstyle = 0
+ttbar.linestyle = '-'
+ttbar.color = "#bc5c61"
+#ttbar.color = "#f6f5f0"
+ttbar.load(filelist_dir + "ttbar", h5_dir_mc) 
+loaded_samples.append(ttbar)
+
+wt = sample.Sample("Wt", "$Wt$")
+wt.scalefactor = lumi_factor
+wt.fillstyle = 0
+wt.linestyle = '-'
+wt.color = "#698bae" 
+wt.load(filelist_dir + "Wt", h5_dir_mc)
+loaded_samples.append(wt)
+#
+#ztt = sample.Sample("Ztt", "$Z \\rightarrow \\tau \\tau$")
+#ztt.scalefactor = lumi_factor
+#ztt.fillstyle = 0
+#ztt.linestyle = '-'
+#ztt.color = "#fecf90" 
+#ztt.load(filelist_dir + "zjets_sherpa_tt", h5_dir_mc)
+#loaded_samples.append(ztt)
+#
+#zll = sample.Sample("Zll", "$Z \\rightarrow \\ell \ell$")
+#zll.scalefactor = lumi_factor
+#zll.fillstyle = 0
+#zll.linestyle = '-'
+#zll.color = "#d1b7a5" 
+#zll.load(filelist_dir + "zjets_sherpa_ll", h5_dir_mc)
+#loaded_samples.append(zll)
+#
+#dib = sample.Sample("Diboson", "$VV \\rightarrow \\ell \\ell \\nu \\nu$")
+#dib.scalefactor = lumi_factor
+#dib.fillstyle = 0
+#dib.linestyle = '-'
+#dib.color = "#785e6f" 
+#dib.load(filelist_dir + "diboson_sherpa_llvv", h5_dir_mc)
+#loaded_samples.append(dib)
+#
+#wjets = sample.Sample("Wjets", "$W + jets$")
+#wjets.scalefactor = lumi_factor
+#wjets.fillstyle = 0
+#wjets.linestyle = '-'
+#wjets.color = "#daadbb" 
+#wjets.load(filelist_dir + "wjets_sherpa", h5_dir_mc)
+#loaded_samples.append(wjets)
+#
+#ttv = sample.Sample("ttV", "$t\\bar{t} + V$")
+#ttv.scalefactor = lumi_factor
+#ttv.fillstyle = 0
+#ttv.linestyle = '-'
+#ttv.color = "#b3e6fd" 
+#ttv.load(filelist_dir + "ttV", h5_dir_mc)
+#loaded_samples.append(ttv)
+#
+#dy = sample.Sample("DY", "Drell-Yan")
+#dy.scalefactor = lumi_factor
+#dy.fillstyle = 0
+#dy.linestyle = '-'
+#dy.color = "#ffd787" 
+#dy.load(filelist_dir + "drellyan_sherpa", h5_dir_mc)
+#loaded_samples.append(dy)
+#
+## data
+#data = sample.Sample("data", "Data")
+#data.is_data = True
+#data.scalefactor = 1.0
+#data.fillstyle = 0
+#data.linestyle = '-'
+#data.color = 'k'
+#data.load(filelist_dir + "data_n0234", h5_dir_data)
+#loaded_samples.append(data)
+
+#signals
+hh0 = sample.Sample("hhSM", "$hh$ SM")
+hh0.is_signal = True
+hh0.scalefactor = lumi_factor * 0.06 * 100
+hh0.fillstyle = 0
+hh0.linestyle = '--'
+hh0.color = 'r'
+hh0.load(filelist_dir + "wwbb_susy2", h5_dir_mc, dsid_select = '342053')
+loaded_samples.append(hh0)
+
+hh1 = sample.Sample("hh800", "X $800$ GeV")
+hh1.is_signal = True
+hh1.scalefactor = lumi_factor * 20
+hh1.fillstyle = 0
+hh1.linestyle = '--'
+hh1.color = 'b'
+hh1.load(filelist_dir + "wwbb_susy2", h5_dir_mc, dsid_select = '343775')
+loaded_samples.append(hh1)
+
+hh2 = sample.Sample("hh1000", "X $1000$ GeV")
+hh2.is_signal = True
+hh2.scalefactor = lumi_factor * 20
+hh2.fillstyle = 0
+hh2.linestyle = '--'
+hh2.color = '#9cfd9d'
+hh2.load(filelist_dir + "wwbb_susy2", h5_dir_mc, dsid_select = "343777")
+loaded_samples.append(hh2)
+
+
+
+#############################################################
+# region definitions
+#############################################################
+logy_regions = []
+
+isSFOS = "((( nMuons == 2 && l0_pt > 25 && l1_pt > 20 ) || ( nElectrons == 2 && l0_pt > 25 && l1_pt > 20 ))"# && mll < 81.2 && mll > 101.2)"#  && (l0_q * l1_q) < 0)"
+
+isEE = "nElectrons==2"
+isMM = "nMuons==2"
+isSF = "nElectrons==2 ||  nMuons==2"
+isDF = "(nElectrons==1 && nMuons==1)"
+#isDFOS = "(nLeptons == 2 && nElectrons == 1 && nMuons == 1 && l0_pt > 25 && l1_pt > 20)" # && (l0_q * l1_q) < 0"
+trigger = "(( year == 2015 && trig_pass2015 == 1 ) || ( year == 2016 && trig_pass2016update == 1 ))"
+
+r = region.Region("wwbbpre", "WW$bb$-pre")
+#r.tcut = "nBJets>=2 && mll>20 && l0_pt>45"
+#r.tcut = "(( %s ) || ( %s )) && %s && nBJets>=2 && mll>20 && mbb>80 && mbb<140" % (isSFOS, isDFOS, trigger)
+#r.tcut = "%s && nBJets>=2 && mll>20 && l0_pt>25 && l1_pt>20 && (mbb<80 || mbb>140) && HT2Ratio>0.98 && MT_1_scaled>800" % ( trigger )#,)isSF, isDF) #, trigger)
+r.tcut = "%s && nBJets>=2 && mll>20 && l0_pt>25 && l1_pt>20 && mbb>80 && mbb<140" % ( trigger )#,)isSF, isDF) #, trigger)
+loaded_regions.append(r)
+
+#############################################################
+# plots
+#############################################################
+variables = {}
+#variables["HT2Ratio"] = { "wwbbpre" : [0.005, 0.98, 1.0] } # if a 4th argument is given, override automatic setting of y-max
+variables["HT2Ratio"] = { "wwbbpre" : [0.05, 0.0, 1.0] } # if a 4th argument is given, override automatic setting of y-max
+variables["nBJets"] = { "wwbbpre" : [1, 0, 12] }
+variables["dRll"] = { "wwbbpre" : [0.1, 0, 5] }
+variables["MT_1_scaled"] = { "wwbbpre" : [40, 0, 1500] }
+variables["mt2_llbb"] = { "wwbbpre" : [10, 60, 400] }
+variables["dRbb"] = { "wwbbpre" : [0.1, 0, 6] }
+variables["l0_pt"] = { "wwbbpre" : [10, 0, 300] }
+variables["l1_pt"] = { "wwbbpre" : [10, 0, 200] }
+
+
+nice_names = {}
+nice_names["HT2Ratio"] = ["$H_{T2}^{R}$"]
+nice_names["nBJets"] = ["# $b-$jets"]
+nice_names["dRll"] = ["$\\Delta R_{\\ell \\ell}$"]
+nice_names["MT_1_scaled"] = ["M$_{T1}$", "GeV"]
+nice_names["mt2_llbb"] = ["$m_{T2}^{\\ell \\ell bb}$", "GeV"]
+nice_names["l0_pt"] = ["Lead lepton $p_{T}$", "GeV"]
+nice_names["l1_pt"] = ["Sub-lead lepton $p_{T}$", "GeV"]
+
+# load the plots 
+for var, bounds in variables.iteritems() :
+    if selected_region not in bounds :
+        print "ERROR selected region (=%s) is not defined in configured variables" % ( selected_region )
+        sys.exit()
+    logy = False
+    if selected_region in logy_regions or do_logy :
+        logy = True
+    p = hist1d.Canvas(logy = logy)
+    p.vartoplot = var
+    p.bounds = bounds[selected_region]
+    name = var.replace("[","").replace("]","").replace("(","").replace(")","")
+    p.name = name
+    y_label_unit = ""
+    if var in nice_names :
+        if len(nice_names[var]) == 2 :
+            y_label_unit = nice_names[var][1]
+    y_label_unit = str(bounds[selected_region][0]) + " " + y_label_unit
+    x_label = var
+    y_label = "Arb. units / %s" % y_label_unit
+    if var in nice_names :
+        x_label = nice_names[var][0]
+    p.labels = [x_label, y_label]
+
+    if selected_region in logy_regions or do_logy :
+        p.logy = True
+
+    p.build_canvas(logy = p.logy)
+
+    loaded_plots.append(p)
+
+    
---
+++ python/analysis/wwbb/shape_plotter.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python
+
+from optparse import OptionParser
+import os
+import sys
+
+import dantrimania.python.analysis.utility.samples.sample as sample
+import dantrimania.python.analysis.utility.samples.region_utils as region_utils
+import dantrimania.python.analysis.utility.samples.sample_utils as sample_utils
+import dantrimania.python.analysis.utility.utils.utils as utils
+import dantrimania.python.analysis.utility.samples.sample_cacher as sample_cacher
+import dantrimania.python.analysis.utility.utils.plib_utils as plib
+
+plt = plib.import_pyplot()
+import numpy as np
+from matplotlib.legend_handler import HandlerLine2D
+
+class histo :
+    def __init__(self, name = "" ) :
+        self.name = name
+        self.color = ""
+        self.label = ""
+
+        self.y = None
+        self.w = None
+        self.e = None
+        self.e_bars = None
+
+def make_histo(pad, sample, filled = False, variable = "", bins = None) :
+
+    chain = sample.chain()
+
+    h = []
+    h_w2 = []
+
+    w = []
+    w2 = []
+
+    for ic, ch in enumerate(chain) :
+        lumi = np.ones(len(ch[variable]))
+        lumi[:] = sample.scalefactor
+        ch_w = lumi * ch['eventweight']
+        w += list(ch_w)
+
+        # sumw2
+        ch_w2 = lumi * ch['eventweight']
+        ch_w2 = ch_w2 ** 2
+        w2 += list(ch_w2)
+
+        # data
+        h += list(ch[variable])
+        h_w2 += list(ch[variable])
+
+    hist = histo(sample.name)
+    y, bin_edges = np.histogram(h, bins = bins, weights = w, normed = 1)
+    bin_centers = 0.5 * (bin_edges[1:] + bin_edges[:-1])
+
+    y_sumw2, _ = np.histogram(h_w2, bins = bins, weights = w2)
+
+    y_err = np.sqrt(y_sumw2)
+
+    histo.y = y
+    histo.e = y_err
+    histo.color = sample.color
+    histo.label = sample.displayname
+
+    alpha = 1.0
+    histtype = 'step'
+    if filled :
+        alpha = 0.5
+        histtype = 'stepfilled'
+    hy, hx, _ = pad.hist(h,
+                bins = bins,
+                weights = w,
+                histtype=histtype,
+                color = sample.color,
+                label = sample.displayname,
+                alpha = alpha,
+                normed = 1, 
+                edgecolor=sample.color,
+                lw = 1.5,
+                ls = sample.linestyle)
+
+    #bars = plt.errorbar(bin_centers, y, yerr = y_err, marker='.',
+    #                        drawstyle='steps-mid', color = sample.color) 
+   # pad.errorbar(bin_centers, y, yerr = y_err, marker='.',
+   #                         drawstyle='steps-mid', color = sample.color) 
+    #histo.e_bars = bars
+
+    return max(hy), min(hy)
+
+def make_shape_plot(plot, region, backgrounds, signals, output_dir) :
+    print 50 * '-'
+    print "make_shape_plot    %s" % plot.vartoplot
+
+    xlow = plot.xlow
+    xhigh = plot.xhigh
+    bw = plot.binwidth
+    nbins = np.arange(xlow, xhigh + bw, bw)
+    pad = plot.pad
+
+    background_histos = []
+    signal_histos = []
+    maxy = -1
+    miny = 1e9
+
+    for bkg in backgrounds :
+        h_maxy, h_miny = make_histo(pad, bkg, variable = plot.vartoplot,
+                                                filled = True, bins = nbins)
+        if h_maxy > maxy : maxy = h_maxy
+        if h_miny < miny  : miny = h_miny
+
+    for sig in signals :
+        h_maxy, h_miny = make_histo(pad, sig, variable = plot.vartoplot,
+                                                filled = False, bins = nbins)
+        if h_maxy > maxy : maxy = h_maxy
+        if h_miny < miny : miny = h_miny
+
+    f = 1.65
+    low = 0.0
+    if plot.logy :
+        f = 100
+        low = 0.01 * miny
+    pad.set_ylim(low, f * maxy)
+
+    pad.legend(loc='best', fontsize=16, frameon=False, ncol=1)
+
+    ########################################
+    # text
+    pad.text(0.05, 0.95, '%s' % region.displayname, size = 18, ha='left', transform=pad.transAxes)
+
+    #########################################
+    # save
+    utils.mkdir_p(output_dir)
+    if not output_dir.endswith("/") :
+        output_dir += "/"
+    save_name = output_dir + "shape_%s_%s.pdf" % ( region.name, plot.vartoplot)
+
+    print " >>> Saving plot to : %s" % os.path.abspath(save_name)
+    plot.fig.savefig(save_name, bbox_inches='tight', dpi=200)
+    
+    
+
+def main() :
+
+    parser = OptionParser()
+    parser.add_option("-c", "--config", default="", help="Configuration file for plotting")
+    parser.add_option("-r", "--region", default="", help="Provide a region selection")
+    parser.add_option("-o", "--output", default="./", help="Provide an output directory for plots (will make it if it does not exist)")
+    parser.add_option("-v", "--var", default="", help="Provide a specific variable to plot")
+    parser.add_option("--logy", default=False, action="store_true", help="Set the plots to have log y-axis")
+    parser.add_option("--cache-dir", default="./sample_cache", help="Directory to place/look for the cached samples")
+
+    (options, args) = parser.parse_args()
+    config = options.config
+    cache_dir = options.cache_dir
+    do_logy = options.logy
+    region = options.region
+    output_dir = options.output
+    select_var = options.var
+
+    print "shape_plotter"
+    print " > config:       %s" % config
+    print " > cache dir:    %s" % cache_dir
+
+    if not utils.file_exists(config) :
+        print "blah"
+        sys.exit()
+
+    global loaded_samples
+    global loaded_regions
+    global selected_region
+    global loaded_plots
+    selected_region = region
+    loaded_samples = []
+    loaded_regions = []
+    loaded_plots = []
+    execfile(config, globals(), locals())
+
+    if len(loaded_samples) == 0 :
+        print "ERROR No loaded samples found in configuration"
+        sys.exit()
+
+    # check region
+    if not region_utils.regions_unique(loaded_regions) :
+        print "ERROR Loaded regions are not unique, here are the counts:"
+        for rname, count in region_utils.region_counts(loaded_regions).iteritems() :
+            print " > %s : %d" % ( rname, count )
+        sys.exit()
+
+    if not region_utils.has_region(loaded_regions, selected_region) :
+        print "ERROR Requested region (=%s) not found in loaded regions:" % ( selected_region )
+        for r in loaded_regions :
+            print str(r)
+        sys.exit()
+
+    region_to_plot = None
+    for r in loaded_regions :
+        if r.name == selected_region :
+            region_to_plot = r
+            break
+
+    # variables
+    variables_found = []
+    for p in loaded_plots :
+        variables_found.append(p.vartoplot)
+    if select_var != "" :
+        if select_var not in variables_found :
+            print "ERROR Requested variable (=%s) not found in configured plots" % select_var
+            sys.exit()
+        tmp_plots = []
+        for p in loaded_plots :
+            if p.vartoplot == select_var :
+                tmp_plots.append(p)
+        loaded_plots = tmp_plots
+
+    # categorize
+    backgrounds, signals, _ = sample_utils.categorize_samples(loaded_samples) 
+
+    # cache
+    cacher = sample_cacher.SampleCacher(cache_dir)
+    cacher.samples = loaded_samples
+    cacher.region = region_to_plot
+    required_variables = sample_utils.get_required_variables(loaded_plots, region_to_plot) 
+    cacher.fields = required_variables
+    print str(cacher)
+    cacher.cache()
+
+    for plot in loaded_plots :
+        make_shape_plot(plot, region_to_plot, backgrounds, signals, output_dir)
+    
+
+#____________________________________________________________
+if __name__ == "__main__" :
+    main()
